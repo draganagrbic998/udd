221256a4cd3c5df548f8d02d0ef0fcdf2c68b1ed
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-11-28 17:15:51 +0100

    highlight

--- backend/src/main/java/com/example/demo/dto/ApplicationSearchResult.java
+++ backend/src/main/java/com/example/demo/dto/ApplicationSearchResult.java
@@ -1,5 +1,8 @@
 package com.example.demo.dto;
 
+import java.util.List;
+import java.util.Map;
+
 import com.example.demo.model.ApplicationIndexUnit;
 
 import lombok.Getter;
@@ -24,9 +27,18 @@ public class ApplicationSearchResult {
 		lastName = indexUnit.getLastName();
 		education = indexUnit.getEducation();
 		letterText = indexUnit.getLetterText();
+
 		adTitle = indexUnit.getAdTitle();
 		cvLocation = indexUnit.getCvLocation();
 		letterLocation = indexUnit.getLetterLocation();
+	}
+
+	public ApplicationSearchResult(ApplicationIndexUnit indexUnit, Map<String, List<String>> highlights) {
+		this(indexUnit);
+		firstName = highlights.getOrDefault("firstName", List.of(firstName)).get(0);
+		lastName = highlights.getOrDefault("lastName", List.of(lastName)).get(0);
+		education = highlights.getOrDefault("education", List.of(education)).get(0);
+		letterText = highlights.getOrDefault("letterText", List.of(letterText)).get(0);
 	}
 
 }
--- backend/src/main/java/com/example/demo/service/ApplicationSearchService.java
+++ backend/src/main/java/com/example/demo/service/ApplicationSearchService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
@@ -22,8 +23,12 @@ public class ApplicationSearchService {
 
 	public List<ApplicationSearchResult> search(QueryBuilder query) {
 		List<ApplicationSearchResult> result = new ArrayList<>();
-		template.search(new NativeSearchQueryBuilder().withQuery(query).build(), ApplicationIndexUnit.class)
-				.forEach(indexUnit -> result.add(new ApplicationSearchResult(indexUnit.getContent())));
+		template.search(new NativeSearchQueryBuilder().withQuery(query)
+				.withHighlightFields(new HighlightBuilder.Field("firstName"), new HighlightBuilder.Field("lastName"),
+						new HighlightBuilder.Field("education"), new HighlightBuilder.Field("letterText"))
+				.build(), ApplicationIndexUnit.class)
+				.forEach(searchHit -> result
+						.add(new ApplicationSearchResult(searchHit.getContent(), searchHit.getHighlightFields())));
 		return result;
 	}
 
