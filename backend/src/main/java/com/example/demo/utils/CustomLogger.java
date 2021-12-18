9fb33a3797939d242908bb832930e5a7e0d41d45
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-12-18 23:04:19 +0100

    minnor

--- backend/src/main/java/com/example/demo/utils/CustomLogger.java
+++ backend/src/main/java/com/example/demo/utils/CustomLogger.java
@@ -7,15 +7,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomLogger {
 
-	private static final Logger LOGGER1 = LoggerFactory.getLogger("logger-1");
-	private static final Logger LOGGER2 = LoggerFactory.getLogger("logger-2");
+	private static final Logger LOGGER = LoggerFactory.getLogger("logger");
 
 	public void storeApplicationFormAccessLog(String city, Double lat, Double lng) {
-		LOGGER1.info("city: " + city + ", lat: " + lat + ", lng: " + lng);
+		LOGGER.info("city: " + city + ", lat: " + lat + ", lng: " + lng);
 	}
 
 	public void storeApplicationSubmitLog() {
-		LOGGER2.info("Application subbmited");
+		LOGGER.info("Application subbmited");
 	}
 
 }
